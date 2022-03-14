9ea3c44808fe371da50a7bb54559891bee083277
ElvanIpeker <eipekerdogan@hotmail.com>
2022-03-14 19:32:37 +0100

    push

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,59 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
-  }
+    this.currentTime= 0
+    this.intervalId=null
+ }
 
-  start(callback) {
-    // ... your code goes here
+ start(callback) {
+    this.intervalId= setInterval(() =>{
+      this.currentTime=this.currentTime+1
+      if(callback){
+        callback()
+      }
+    },1000)
+
+    
+
   }
 
   getMinutes() {
-    // ... your code goes here
+    
+    return Math.floor(this.currentTime/60)
+
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime%60)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if(value<10)
+    { return "0"+ value 
+
+    }
+    else{
+     return "" + value
+     }
+
   }
 
   stop() {
-    // ... your code goes here
+   clearInterval(this.intervalId) 
+   this.currentTime=0
+   this.intervalId=null
   }
 
   reset() {
-    // ... your code goes here
+    this.stop()
+
+    this.start()
   }
 
   split() {
-    // ... your code goes here
+    let minutes=this.getMinutes()
+    let seconds=this.getSeconds()
+    return this.computeTwoDigitNumber(minutes) + ":" +this.computeTwoDigitNumber(seconds)
+
   }
 }
 
